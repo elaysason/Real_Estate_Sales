783e04c8d031a49edac39a0deaf56d5f364e8cd4
elaysason <elaysason@gmail.com>
2024-06-29 09:54:01 +0300

    fixed comments and finial messages

--- scrapping.py
+++ scrapping.py
@@ -12,7 +12,7 @@ import sys
 from geopy.geocoders import Photon
 
 
-def search_website():
+def search_website(search_place):
     # Set up Chrome in headless mode
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -20,12 +20,12 @@ def search_website():
     driver = webdriver.Chrome(options=chrome_options)
 
     driver.get(
-        "https://www.nadlan.gov.il/")  # Replace "https://example.com" with the URL of the website you want to search
+        "https://www.nadlan.gov.il/")
 
     # Perform search
     search_box = driver.find_element(By.XPATH,
-                                     '//*[@id="SearchString"]')  # Replace "input.search-board" with the correct CSS selector for your search box
-    search_box.send_keys("כפר גנים ג")  # Replace "Your search query here" with your actual search query
+                                     '//*[@id="SearchString"]')
+    search_box.send_keys(search_place)  # Replace "Your search query here" with your actual search query
     search_box.send_keys(Keys.RETURN)
 
     search_button = driver.find_element(By.XPATH,
@@ -59,7 +59,7 @@ def search_website():
     driver.quit()
     return new_sale, latest_sale,url
 
-def send_email1(sale_details,url):
+def sale_email(sale_details, url):
     # Email configuration
     sender_email = "elaysason123@gmail.com"
     receiver_emails = ["moshesason100@gmail.com","sasonsarit1@gmail.com"]
@@ -72,16 +72,13 @@ def send_email1(sale_details,url):
         message["To"] = receiver_email
         message["Subject"] = subject
 
-        # Create HTML content for the email
         body = f"בתאריך {sale_details[0]} נמכרה {sale_details[3]} בכתובת {sale_details[1]} עם {sale_details[4]} חדרים בקומה {sale_details[5]} בשטח {sale_details[6]} ובמחיר {sale_details[7]} .שקל " + "\n לעוד פרטים ולאתר: "+str(url)
-
         # Set up the email message
         message = MIMEMultipart("related")
         message["From"] = sender_email
         message["To"] = receiver_email
         message["Subject"] = subject
 
-        # Create HTML content for the email with right-to-left direction
         html_content = f"""
         <html>
         <head>
@@ -101,15 +98,15 @@ def send_email1(sale_details,url):
         # Attach HTML content to the email
         message.attach(MIMEText(html_content, "html"))
 
-        # Connect to the SMTP server
-        server = smtplib.SMTP("smtp.gmail.com", 587)  # Replace "smtp.example.com" with your SMTP server and port
+        # Connect to the email server
+        server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login(sender_email, password)
 
         # Send the email
         server.sendmail(sender_email, receiver_email, message.as_string())
 
-        # Close the connection
+        # Closing the connection
         server.quit()
 
 if __name__ == "__main__":
@@ -130,9 +127,10 @@ if __name__ == "__main__":
 #       print("Longitude:", longitude)
 #   else:
 #       print("Address not found.")
-    new_sale, latest_sale,url = search_website()
+    new_sale, latest_sale,url = search_website("כפר גנים ג")
 
     if new_sale:
-        send_email1(latest_sale,url)
+        sale_email(latest_sale, url)
         print('Finished the run, new sale recorded and email sent.')
-    print('Finished the run, no new sale')
+    else:
+        print('Finished the run, no new sale')
