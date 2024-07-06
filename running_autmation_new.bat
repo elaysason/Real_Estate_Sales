728c888a6203f015cb3d6b863ec7d8c8baa92064
elaysason <elaysason@gmail.com>
2024-07-06 10:29:32 +0300

    fixing autmation file

--- running_autmation_new.bat
+++ running_autmation_new.bat
@@ -1,4 +1,3 @@
 @echo off
-"C:\Users\User\PycharmProjects\scraping_sales\.venv\Scripts\python.exe" "C:\Users\User\PycharmProjects\scraping_sales\sales_notification.py""C:\Users\User\PycharmProjects\scraping_sales\config.json"
-pause
+"C:\Users\User\PycharmProjects\scraping_sales\.venv\Scripts\python.exe" "C:\Users\User\PycharmProjects\scraping_sales\sales_notification.py" "C:\Users\User\PycharmProjects\scraping_sales\config.json"
 pause
