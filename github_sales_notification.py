fd2094664063c7fe5f11e5021984d63047699623
Elay Sason <89357293+elaysason@users.noreply.github.com>
2025-05-30 08:37:41 +0300

    Update github_sales_notification.py

--- github_sales_notification.py
+++ github_sales_notification.py
@@ -12,7 +12,6 @@ import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
 from utils import validate_parameters, validate_config, get_driver
 
 
